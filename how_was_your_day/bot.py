8051900bbffc74a9adec2cf8efb457fd6701dabf
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2023-01-15 02:52:09 +0400

    now name is in resp

--- how_was_your_day/bot.py
+++ how_was_your_day/bot.py
@@ -37,6 +37,19 @@ telegram_token = os.getenv("TELEGRAM_API_KEY")
 bot = telebot.TeleBot(telegram_token, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
 
+def get_user_info(message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    user_username = message.from_user.username
+    user_info = {
+        "user_id": user_id,
+        "user_name": user_name,
+        "user_username": user_username,
+        "user_last_name": last_name
+    }
+    return user_info
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, msgs.welcome_msg)
@@ -48,6 +61,7 @@ def send_creator_info(message):
 @bot.message_handler(commands=['query'])
 def send_query(message):
     prompt = message.text
+    user_info = get_user_info(message)
     logging.info(f"User query input: {prompt} with command: query")
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=100)
     logging.info(f"Bot response: {response.choices[0].text}")
@@ -56,8 +70,9 @@ def send_query(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     user_input = message.text
+    user_info = get_user_info(message)
     logging.info(f"User query input: {user_input}.")
-    base_prompt = f"User comes and says: {user_input} \n You want to say something nice to user, support him. Want to make he or she happy and reply with:"
+    base_prompt = f"Person with name {user_info['user_name']} comes and says: {user_input} \n You want to say something nice to user, support him. Want to make he or she happy and reply with:"
     response = openai.Completion.create(model="text-davinci-003", prompt=base_prompt, temperature=0.5, max_tokens=100)
     logging.info(f"Bot response: {response.choices[0].text}")
     bot.reply_to(message, response.choices[0].text)
